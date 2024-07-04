a69ab13feddbe7fb6f12f3b1d93dda18ecedef30
Timothée Mazzucotelli <dev@pawamoy.fr>
2024-07-04 20:02:53 +0200

    feat: Add feedback system using Giscus

--- extensions.py
+++ extensions.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+
 import re
 import subprocess
 import unicodedata
 from datetime import date
 
+from copier_templates_extensions import ContextHook
 from jinja2.ext import Extension
 
 
@@ -37,3 +40,39 @@ class CurrentYearExtension(Extension):
     def __init__(self, environment):
         super().__init__(environment)
         environment.globals["current_year"] = date.today().year
+
+
+class GitHubIDsforGiscusExtension(ContextHook):
+    repo_placeholder = "REPLACE WITH REPOSITORY NODE ID"
+    category_placeholder = "REPLACE WITH DISCUSSION CATEGORY ID"
+    update = False
+    query = """                     
+    {
+        repository(owner: "%(owner)s", name: "%(name)s") {
+            discussionCategories(first: 100) {
+                nodes {
+                    id
+                    name
+                }
+            }
+        }
+    }
+    """
+
+    repo_id: str | None = None
+    category_id: str | None = None
+
+    def hook(self, context):
+        repository_namespace = context["repository_namespace"]
+        repository_name = context["repository_name"]
+    
+        if self.repo_id is None:
+            command = f"gh api repos/{repository_namespace}/{repository_name} --jq .node_id"
+            self.repo_id = subprocess.getoutput(command).strip() or self.repo_placeholder
+        context["giscus_repo_id"] = self.repo_id
+
+        if self.category_id is None:
+            jq_filter = "--jq '.data.repository.discussionCategories.nodes[] | select(.name == \"Documentation\") | .id'"
+            command = f"gh api graphql -f query='{self.query}' {jq_filter}" % {"owner": repository_namespace, "name": repository_name}
+            self.category_id = subprocess.getoutput(command).strip() or self.category_placeholder
+        context["giscus_discussion_category_id"] = self.category_id
